9ee226ae60498f3abc518da3e75a9f3092c67f9a
EJCalderon <emiliojaviercalderon@gmail.com>
2022-03-02 22:38:43 -0600

    end of day

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -48,3 +48,23 @@ const thoughtsController = {
         })
         .catch(err => res.status(400).json(err));
     },
+
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete
+    }
