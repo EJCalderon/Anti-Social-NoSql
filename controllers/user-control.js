d7ce8141ae6fbca240da7bae133310267c2db548
EJCalderon <emiliojaviercalderon@gmail.com>
2022-03-04 11:33:10 -0600

    controlls

--- controllers/user-control.js
+++ controllers/user-control.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const { Users } = require('../models');
 
 const userControllers = {
@@ -17,4 +18,76 @@ const userControllers = {
             res.status(400).json(err);
         });
     },
-}
+    getUsersById({params}, res) {
+        Users.findOne({_id: params.id })
+        .populate({path: 'thoughts', select:'-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'None'});
+                return;
+            }
+            res.json(dbUserData)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            })
+        },
+        updateUsers({params, body}, res) {
+            Users.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+            .then(dbThoughtsData => {
+                if(!dbThoughtsData) {;
+                res.status(404).json({message: 'None'});
+            return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err))
+    },
+    deleteUsers({params}, res) {
+        Users.findOneAndDelete({_id: params.id})
+            .then(dbThoughtData => {
+                if(!dbThoughtsData){
+                    res.status(404).json({message: 'None'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    addriend({params}, res) {
+        Users.findOneAndDelete({_id: params.id}, {$push: {friends: params.friendId}}, {new: true})
+        .populate({path: 'friends', select: ('-__v')})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: 'none'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));  
+    },
+
+
+
+    deleteFriend({params}, res) {
+        Users.findOneAndDelete({_id: params.id}, {$push: {friends: params.friendId}}, {new: true})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({message: '404'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));   
+    }
+
+};
+
+module.exports = userControllers;
